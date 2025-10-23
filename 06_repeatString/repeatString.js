5f758a7cce35e8114258c944379b36c85e3fc9b9
louisonrails <237607115+louisonrails@users.noreply.github.com>
2025-10-23 11:18:29 -0300

    Completed 06_repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(greeting, number) {
+    let output = "";
+    for(let i = 0; i< number; i++){
+        output += greeting;
+    }
+    return output;
 };
 
 // Do not edit below this line
