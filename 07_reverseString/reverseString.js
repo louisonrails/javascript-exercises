1c60dfaea9fd7fb1827be64df5294f6c51a74e45
louisonrails <237607115+louisonrails@users.noreply.github.com>
2025-10-23 11:27:18 -0300

    Completed Exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(stringValue) {
+    let stringLetters = stringValue.split('');
+    let output = "";
+    for(let i = stringLetters.length - 1; i >= 0; i--){
+        output += stringLetters[i];
+    }
+    return output;
 };
 
 // Do not edit below this line
