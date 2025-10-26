51c5c2279345cd74851b6006c062f33900ad70fb
louisonrails <237607115+louisonrails@users.noreply.github.com>
2025-10-25 22:11:42 -0300

    Completed exercise 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+  numbers.forEach(number => {
+    sum += number;
+  });
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let value = 1;
+  
+  numbers.forEach(number => {
+    value *= number;
+  });
+
+  return value;
 
 };
 
-const power = function() {
-	
+const power = function(a , b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let value = 1;
+  for(let i = number; i > 0; i--){
+    value *= i;
+  }
+  return value;
 };
 
 // Do not edit below this line
